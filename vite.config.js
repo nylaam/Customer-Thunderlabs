9b80505f3da2d7abcd27e3dd15f1d353a9dea0f4
Nayla Nur Amalia <nayla_amalia_30rpl@student.smktelkom-mlg.sch.id>
2024-03-03 16:17:38 +0700

    add vite config

---
+++ vite.config.js
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const manifestForPlugin = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"Thunderlab Customer",
+    short_name:"Thunderlab Cust",
+    description:"Customer management platform used to manage payment data",
+    icons:[{
+      src: '/android-chrome-192x192.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src:'/android-chrome-512x512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: '/apple-touch-icon.png',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'apple touch icon',
+    },
+    {
+      src: '/maskable_icon.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
+  ],
+  theme_color:'#181818',
+  background_color:'#e0cc3b',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  },
+};
+export default defineConfig({
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
